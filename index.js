fb073345b80eeef2adaa3e893a2732d7dc2a40d1
Vidya Raman <vidyaraman@Vidyas-MacBook-Pro.local>
2024-07-25 20:40:33 +0530

    Demo devops commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the DevOps Demo today!');
+  res.send('Welcome to the DevOps Demo today! It is Thursday!');
 });
 
 app.listen(port, () => {
